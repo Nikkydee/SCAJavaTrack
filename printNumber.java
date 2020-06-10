498d35847ead35b5b9e4a90c778a06f2a24523ac
Nikkydee <43099966+Nikkydee@users.noreply.github.com>
2020-06-10 22:23:43 +0100

    My First SCA Exercise

---
+++ printNumber.java
@@ -0,0 +1,37 @@
+public class printNumber {
+
+    public static void main(String[] args){
+
+        int num = 100;
+       for( int i = 1; i <=num; i++){
+           System.out.println(i);
+
+        }
+    }
+
+
+}
+
+
+/*Solution
+
+
+
+1. Instance variable is a variable declared inside the class of the variable but outsidy the body of a method
+For example:
+public class instance{
+float = 0.5f //instance variable
+static  int n = 3// static variable
+
+public static void main(String[]args){
+double k = 5// local variable
+
+}
+
+}
+ Another name for Instance variable is non-static field
+
+2. Static field is a variable that is held in common for all instance of a class . It also called called class variable
+3. The primitive data type supported by Java are
+	boolean, char, byte, short, int, long, float and double.*/
+
---
+++ sumnaturalNumber.java
@@ -0,0 +1,18 @@
+public class sumnaturalNumber {
+
+    public static class addNumber {
+
+        public static void main(String[] args){
+            int finalNum =10;
+            int sum = 0;
+            for( int i = 1; i <=finalNum; i++){
+
+               sum= sum +i;
+
+            }
+            System.out.println(sum);
+        }
+
+
+    }
+}
